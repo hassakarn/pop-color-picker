95bd5031215936cb522683d0de960135ed8db8fe
Hassakarn C <has606@gmail.com>
2015-01-01 01:56:32 +0700

    first commit

---
+++ PopColorPicker/Views/ColorPickerDeletePanelView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace PopColorPicker
+{
+    public class ColorPickerDeletePanelView: UIView
+    {
+        public ColorPickerDeletePanelView()
+            : base()
+        {
+            BackgroundColor = UIColor.Black.ColorWithAlpha(0.5f);
+        }
+    }
+}
+
